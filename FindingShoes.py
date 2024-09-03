c1ced4d41a93fc7edb34f94da6375df4acd492cc
Ajesh patel <ajeshpatel10@gmail.com>
2024-09-03 10:09:59 +0530

    Blackjack

--- FindingShoes.py
+++ FindingShoes.py
@@ -1,26 +1,27 @@
 """ Finding Shoes
 
-Chef has NN friends. Chef promised that he would gift a pair of shoes
-(consisting of one left shoe and one right shoe) to each of his NN friends. 
+Chef has N friends. Chef promised that he would gift a pair of shoes
+(consisting of one left shoe and one right shoe) to each of his N friends. 
 Chef was about to go to the marketplace to buy shoes, but he suddenly remembers that he already had MM left shoes.
 
 What is the minimum number of extra shoes that Chef will have to buy to ensure that
-he is able to gift a pair of shoes to each of his NN friends?
+he is able to gift a pair of shoes to each of his N friends?
 
-For example, if N=2N=2, M=4M=4, then Chef already has 44 left shoes, 
-so he must buy 22 extra right shoes to form 22 pairs of shoes.
+For example, if N=2N=2, M=4M=4, then Chef already has 4 left shoes, 
+so he must buy 2 extra right shoes to form 2 pairs of shoes.
 
-Therefore Chef must buy at least 22 extra shoes to ensure that he is able to get N=2N=2 pairs of shoes.
+Therefore Chef must buy at least 2 extra shoes to ensure that he is able to get N=2 pairs of shoes.
 
 Input Format:
 
-    The first line contains a single integer TT - the number of test cases. Then the test cases follow.
-    The first line of each test case contains two integers NN and MM - the number of Chef's friends and the number of left shoes Chef has.
+    The first line contains a single integer T - the number of test cases. Then the test cases follow.
+    The first line of each test case contains two integers NN and MM - the number of Chef's friends
+    and the number of left shoes Chef has.
 
 Output Format:
 
 For each test case, output the minimum number of extra shoes that Chef will have to buy to ensure
-that he is able to get NN pairs of shoes. 
+that he is able to get N pairs of shoes. 
 
 Constraints
 
@@ -43,11 +44,11 @@ Explanation:
 
 Test Case 1: Discussed in the problem statement
 
-Test Case 2: Chef initially has no left shoes. He must buy 66 more left shoes and 66 more
-right shoes to form 66 pairs of shoes.
+Test Case 2: Chef initially has no left shoes. He must buy 6 more left shoes and 6 more
+right shoes to form 6 pairs of shoes.
 
-Test Case 3: Chef initially has 33 left shoes. He must buy 11 more left shoe and 44 more right shoes
-to form 44 pairs of shoes. """
+Test Case 3: Chef initially has 3 left shoes. He must buy 1 more left shoe and 4 more right shoes
+to form 4 pairs of shoes. """
 
 # -----------SOLUTION------------
 # cook your dish here
