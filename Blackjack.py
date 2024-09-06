45ea95cd8bea1035b24258881e18cbb8f6deddf9
Ajesh patel <ajeshpatel10@gmail.com>
2024-09-06 23:45:33 +0530

    Blackjack

--- Blackjack.py
+++ Blackjack.py
@@ -1,8 +1,8 @@
 """
 Blackjack
 
-Chef is playing a variant of Blackjack, where 33 numbers are drawn and each number lies between
-1 and 10 (with both 11 and 1010 inclusive). Chef wins the game when the sum of these 3 numbers is exactly 21.
+Chef is playing a variant of Blackjack, where 3 numbers are drawn and each number lies between
+1 and 10 (with both 11 and 10 inclusive). Chef wins the game when the sum of these 3 numbers is exactly 21.
 
 Given the first two numbers A and B, that have been drawn by Chef, what should be 3-rd number
 that should be drawn by the Chef in order to win the game?
@@ -18,8 +18,8 @@ Output Format
 For each testcase, output the 33-rd number that should be drawn by the Chef in order to win the game. 
 Output −1 if it is not possible for the Chef to win the game.
 Constraints:
-    1≤T≤1001≤T≤100
-    1≤A,B≤101≤A,B≤10
+    1≤T≤100
+    1≤A,B≤10
 
 Sample 1:
 Input:
@@ -34,10 +34,10 @@ Output:
 
 Explanation:
 
-Test case 11: The first two numbers are 1 and 10. If the third number will be 10, 
-the resulting sum will be 1+10+10=21. So Chef will win the game if the third number is 1010.
+Test case 1: The first two numbers are 1 and 10. If the third number will be 10, 
+the resulting sum will be 1+10+10=21. So Chef will win the game if the third number is 10.
 
-Test case 22: The first two numbers are 1 and 5. There is no number between 1 and 10, 
+Test case 2: The first two numbers are 1 and 5. There is no number between 1 and 10, 
 that can make the resulting sum 21. Hence, the answer will be −1 in this test case.
 """
 # -----------------------SOLUTION------------------
