63086c087a4c236c97fcd3820cc6a713eeade350
Ajesh patel <ajeshpatel10@gmail.com>
2024-10-29 21:47:40 +0530

    Zero String

--- 1000 t0 14000 difficulty problems/Zero String.py
+++ 1000 t0 14000 difficulty problems/Zero String.py
@@ -2,33 +2,34 @@
 
 Zero String
 
-You are given a binary string SS of length NN. You are allowed to perform the following types of operations on string SS:
+You are given a binary string SS of length NN. You are allowed to perform the following types of operations
+on string S:
 
-    Delete any one character from SS, and concatenate the remaining parts of the string. For example, if we delete the third character of S=1101S=1101, it becomes S=111S=111.
-    Flip all the characters of SS. For example, if we flip all character of S=1101S=1101, it becomes S=0010S=0010.
+    Delete any one character from S, and concatenate the remaining parts of the string. For example,
+    if we delete the third character of S=1101, it becomes S=111.
+    Flip all the characters of S. For example, if we flip all character of S=1101, it becomes S=0010.
 
-Given that you can use either type of operation any number of times, find the minimum number of operations required to make all characters of the string SS equal to 00.
-Input Format
+Given that you can use either type of operation any number of times, find the minimum number of operations
+required to make all characters of the string S equal to 0.
 
-    The first line of input will contain a single integer TT, denoting the number of test cases.
+Input Format:
+    The first line of input will contain a single integer T, denoting the number of test cases.
     Each test case consists of multiple lines of input.
-        The first line of each test case contains an integer NN — the length of the string.
+        The first line of each test case contains an integer N — the length of the string.
         The next line contains a binary string SS of length NN.
 
-Output Format
+Output Format:
+For each test case, output on a new line, the minimum number of operations required to make all characters
+of the string S equal to 0.
 
-For each test case, output on a new line, the minimum number of operations required to make all characters of the string SS equal to 00.
-Constraints
-
-    1≤T≤20001≤T≤2000
-    1≤N≤1051≤N≤105
-    SS contains 00 and 11 only.
-    The sum of NN over all test cases won't exceed 2⋅1052⋅105.
+Constraints:
+    1≤T≤2000
+    1≤N≤105
+    S contains 0 and 1 only.
+    The sum of N over all test cases won't exceed 2⋅105.
 
 Sample 1:
-Input
-Output
-
+Input:
 4
 2
 01
@@ -39,6 +40,7 @@ Output
 4
 0000
 
+Output:
 1
 2
 1
@@ -46,18 +48,21 @@ Output
 
 Explanation:
 
-Test case 11: You can use one operation to delete the second character of the string SS. Thus, the string becomes 00. Note that all characters of this string are 00 and thus, it satisfies the conditions.
+Test case 1: You can use one operation to delete the second character of the string S. Thus, the string becomes 0.
+Note that all characters of this string are 0 and thus, it satisfies the conditions.
 
-Test case 22: You can perform the following operations:
+Test case 2: You can perform the following operations:
 
-    Operation 11: Flip all characters of the string. Thus, string becomes 010010.
-    Operation 22: Delete the second character of the string. Thus, string becomes 0000.
+    Operation 1: Flip all characters of the string. Thus, string becomes 010.
+    Operation 2: Delete the second character of the string. Thus, string becomes 00.
 
-Note that we have obtained a string having all characters as 00 in two operations. It can be shown that this is the minimum number of operations required.
+Note that we have obtained a string having all characters as 0 in two operations. It can be shown that
+this is the minimum number of operations required.
 
-Test case 33: You can use one operation to flip all characters of the string SS. Thus, the string becomes 000000. Note that all characters of this string are 00 and thus, it satisfies the conditions.
+Test case 3: You can use one operation to flip all characters of the string S. Thus, the string becomes 000. 
+Note that all characters of this string are 0 and thus, it satisfies the conditions.
 
-Test case 44: The existing string satisfies the conditions. Thus, we require zero operations.
+Test case 4: The existing string satisfies the conditions. Thus, we require zero operations.
 """
 # -----------------------------------------------SOLUTION-----------------------------------------------------
 # cook your dish here
