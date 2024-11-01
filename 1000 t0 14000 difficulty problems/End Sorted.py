049f7e9cdce14b7a920c1dfd12559da9f4a18682
Ajesh patel <ajeshpatel10@gmail.com>
2024-11-01 18:14:37 +0530

    End Sorted

--- 1000 t0 14000 difficulty problems/End Sorted.py
+++ 1000 t0 14000 difficulty problems/End Sorted.py
@@ -1,34 +1,32 @@
 """
 End Sorted
 
-Chef considers a permutation PP of {1,2,3,…,N}{1,2,3,…,N} End Sorted if and only if P1=1P1​=1 and PN=NPN​=N.
+Chef considers a permutation P of {1,2,3,…,N} End Sorted if and only if P1​=1 and PN​=N.
 
-Chef is given a permutation PP.
+Chef is given a permutation P.
 
-In one operation Chef can choose any index i (1≤i≤N−1)i (1≤i≤N−1) and swap PiPi​ and Pi+1Pi+1​. Determine the minimum number of operations required by Chef to make the permutation PP End Sorted.
+In one operation Chef can choose any index i (1≤i≤N−1) and swap Pi​ and Pi+1​. Determine the minimum number
+of operations required by Chef to make the permutation P End Sorted.
 
-Note: An array PP is said to be a permutation of {1,2,3,…,N}{1,2,3,…,N} if PP contains each element of {1,2,3,…,N}{1,2,3,…,N} exactly once.
-Input Format
+Note: An array P is said to be a permutation of {1,2,3,…,N} if PP contains each element of {1,2,3,…,N} exactly once.
 
-    The first line of input will contain a single integer TT, denoting the number of test cases.
+Input Format:
+    The first line of input will contain a single integer T, denoting the number of test cases.
     Each test case consists of two lines of input.
-        The first line of each test case contains a single integer NN, denoting the length of the permutation PP.
-        The second line contains NN space-separated integers P1,P2,P3,…,PNP1​,P2​,P3​,…,PN​, denoting the permutation PP.
+        The first line of each test case contains a single integer N, denoting the length of the permutation P.
+        The second line contains N space-separated integers P1​,P2​,P3​,…,PN​, denoting the permutation PP.
 
-Output Format
+Output Format:
+For each test case, output minimum number of operations required by Chef to make the permutation P End Sorted.
 
-For each test case, output minimum number of operations required by Chef to make the permutation PP End Sorted.
-Constraints
-
-    1≤T≤10001≤T≤1000
-    2≤N≤1052≤N≤105
-    PP is a permutation of {1,2,3,…N}{1,2,3,…N}
-    The sum of NN over all test cases does not exceed 3⋅1053⋅105.
+Constraints:
+    1≤T≤1000
+    2≤N≤105
+    P is a permutation of {1,2,3,…N}
+    The sum of N over all test cases does not exceed 3⋅10**5.
 
 Sample 1:
-Input
-Output
-
+Input:
 4
 4
 1 3 2 4
@@ -39,6 +37,7 @@ Output
 3
 2 1 3
 
+Output:
 0
 3
 1
