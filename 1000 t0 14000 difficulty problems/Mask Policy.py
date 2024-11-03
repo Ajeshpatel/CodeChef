35c20be141f6701df451bee9bc8694ef6a43b38c
Ajesh patel <ajeshpatel10@gmail.com>
2024-11-03 20:25:35 +0530

    Mask Policy

--- 1000 t0 14000 difficulty problems/Mask Policy.py
+++ 1000 t0 14000 difficulty problems/Mask Policy.py
@@ -3,34 +3,37 @@ Mask Policy
 
 A city has been infected by a contagious virus.
 
-In a survey, it was found that AA out of the NN people living in the city are currently infected. It has been observed that the only way for a person to get infected is if he comes in contact with an already infected person, and both of them are NOT wearing a mask.
+In a survey, it was found that A out of the N people living in the city are currently infected. It has been
+observed that the only way for a person to get infected is if he comes in contact with an already infected
+person, and both of them are NOT wearing a mask.
 
-The mayor of the city wants to make a new Mask Policy and find out the minimum number of people that will be required to wear a mask to avoid the further spread of the virus. Help the mayor in finding this number.
+The mayor of the city wants to make a new Mask Policy and find out the minimum number of people that 
+will be required to wear a mask to avoid the further spread of the virus. Help the mayor in finding this number.
 
 Note: The only aim of the mayor is to stop virus spread, not to mask every infected person.
-Input Format
 
-    The first line contains TT - number of test cases. Then the test cases follow.
-    The first and only line of each test case contains two integers NN and AA - the total number of people living in the city and the number of people already affected by the virus respectively.
+Input Format:
+    The first line contains T - number of test cases. Then the test cases follow.
+    The first and only line of each test case contains two integers N and A - the total number of people 
+    living in the city and the number of people already affected by the virus respectively.
 
-Output Format
+Output Format:
+For each test case, output the minimum number of people that will be required to wear a mask so as to
+curb the virus spread.
 
-For each test case, output the minimum number of people that will be required to wear a mask so as to curb the virus spread.
-Constraints
-
-    1≤T≤1051≤T≤105
-    2≤N≤4002≤N≤400
-    1≤A<N1≤A<N
+Constraints:
+    1≤T≤105
+    2≤N≤400
+    1≤A<N
 
 Sample 1:
-Input
-Output
-
+Input:
 3
 2 1
 3 2
 3 1
 
+Output:
 1
 1
 1
