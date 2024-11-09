c4f44301bc752357263ec039751b562698d1d284
Ajesh patel <ajeshpatel10@gmail.com>
2024-11-09 21:39:46 +0530

    Three Friends

--- 1000 t0 14000 difficulty problems/Three Friends.py
+++ 1000 t0 14000 difficulty problems/Three Friends.py
@@ -1,52 +1,53 @@
 """
 Three Friends
 
-###Read problems statements Mandarin , Bengali , Hindi , Russian and Vietnamese as well.
-
 There are three friends; let's call them A, B, C. They made the following statements:
 
-    A: "I have xx Rupees more than B."
-    B: "I have yy rupees more than C."
-    C: "I have zz rupees more than A."
+    A: "I have x Rupees more than B."
+    B: "I have y rupees more than C."
+    C: "I have z rupees more than A."
 
-You do not know the exact values of x,y,zx,y,z. Instead, you are given their absolute values, i.e. X=∣x∣X=∣x∣, Y=∣y∣Y=∣y∣ and Z=∣z∣Z=∣z∣. Note that xx, yy, zz may be negative; "having −r−r rupees more" is the same as "having rr rupees less".
+You do not know the exact values of x,y,z. Instead, you are given their absolute values, 
+i.e. X=∣x∣, Y=∣y∣ and Z=∣z∣. Note that x, y, z may be negative; "having −r rupees more" is the same as 
+"having rr rupees less".
 
 Find out if there is some way to assign amounts of money to A, B, C such that all of their statements are true.
-Input
 
-    The first line of the input contains a single integer TT denoting the number of test cases. The description of TT test cases follows.
-    The first and only line of each test case contains three space-separated integers XX, YY and ZZ.
+Input:
+    The first line of the input contains a single integer T denoting the number of test cases. 
+    The description of T test cases follows.
+    The first and only line of each test case contains three space-separated integers X, Y and Z.
 
-Output
+Output:
+For each test case, print a single line containing the string "yes" if the presented scenario is possible or
+"no" otherwise (without quotes).
 
-For each test case, print a single line containing the string "yes" if the presented scenario is possible or "no" otherwise (without quotes).
-Constraints
+Constraints:
+    1≤T≤1,000
+    1≤X,Y,Z≤1,000
 
-    1≤T≤1,0001≤T≤1,000
-    1≤X,Y,Z≤1,0001≤X,Y,Z≤1,000
-
-Subtasks
-
+Subtasks:
 Subtask #1 (30 points):
 
-    1≤T≤301≤T≤30
-    1≤X,Y,Z≤31≤X,Y,Z≤3
+    1≤T≤30
+    1≤X,Y,Z≤3
 
 Subtask #2 (70 points): original constraints
-Sample 1:
-Input
-Output
 
+Sample 1:
+Input:
 2
 1 2 1
 1 1 1
 
+Output:
 yes
 no
 
 Explanation:
 
-Example 1: One possible way to satisfy all conditions is: A has 1010 rupees, B has 99 rupees and C has 1111 rupees. Therefore, we have x=1x=1, y=−2y=−2, z=1z=1.
+Example 1: One possible way to satisfy all conditions is: A has 10 rupees, B has 9 rupees and C has 11 rupees. 
+Therefore, we have x=1, y=−2, z=1.
 
 Example 2: There is no way for all conditions to be satisfied.
 """
