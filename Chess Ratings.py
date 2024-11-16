bdf180f16521322d0a9679a4e182ba61b5bcc4a6
Ajesh patel <ajeshpatel10@gmail.com>
2024-11-16 22:39:26 +0530

    Chess Ratings

--- Chess Ratings.py
+++ Chess Ratings.py
@@ -14,8 +14,8 @@ Output Format
 For each test case, output the minimum number of games that Alice needs to win in order to make her rating greater than or equal to YY.
 Constraints
 
-    1≤T≤1041≤T≤104
-    1≤X≤Y≤1041≤X≤Y≤104
+    1≤T≤104
+    1≤X≤Y≤104
 
 Sample 1:
 Input:
@@ -24,6 +24,7 @@ Input:
 10 17
 10 18
 10 19
+
 Output:
 0
 1
@@ -32,35 +33,33 @@ Output:
 
 Explanation:
 
-Test case 11: Since Alice's current rating XX is already equal to her desired rating YY, 
+Test case 1: Since Alice's current rating X is already equal to her desired rating Y, 
 she doesn't need to win any game.
 
-Test case 22: Alice's current rating is 1010. After winning 11 game, her rating will become 10+8=1810+8=18, 
-which is greater than her desired rating of 1717. Thus, she has to win at least 11 game.
+Test case 2: Alice's current rating is 10. After winning 11 game, her rating will become 10+8=18, 
+which is greater than her desired rating of 17. Thus, she has to win at least 1 game.
 
-Test case 33: Alice's current rating is 1010. After winning 11 game, her rating will become 10+8=1810+8=18, 
-which is equal to her desired rating of 1818. Thus, she has to win at least 11 game.
+Test case 3: Alice's current rating is 10. After winning 11 game, her rating will become 10+8=18, 
+which is equal to her desired rating of 18. Thus, she has to win at least 1 game.
 
-Test case 44: Alice's current rating is 1010. After winning 11 game, her rating will become 1818, 
-which is less than her desired rating of 1919. She will need to win one more game in order to 
-make her rating 2626, which is greater than 1919. Thus, she has to win at least 22 games.
+Test case 4: Alice's current rating is 10. After winning 1 game, her rating will become 18, 
+which is less than her desired rating of 19. She will need to win one more game in order to 
+make her rating 26, which is greater than 1919. Thus, she has to win at least 2 games.
 """
 
-# -------------SOLUTION--------------
+# --------------------------------------------SOLUTION------------------------------------------------
 
 #method:1
 # cook your dish here
-"""
-t = int(input())
+# t = int(input())
 
-for _ in range(t):
-    x,y = map(int, input().split())
+# for _ in range(t):
+#     x,y = map(int, input().split())
     
-    diff = x-y
-    req_game = diff//8
+#     diff = x-y
+#     req_game = diff//8
     
-    print(-req_game)
-"""
+#     print(-req_game)
 
 
 # method:2
